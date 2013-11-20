0dffd0c851f6d141123ed80d25e5009ab94f6e05
Mingcai SHEN <archsh@gmail.com>
2013-11-20 22:07:34 +0800

    Updated example code.

--- examples/HelloWorld.py
+++ examples/HelloWorld.py
@@ -3,6 +3,7 @@ from restlet.application import Application
 from restlet.handler import RestletHandler, encoder, decoder, route
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Sequence
+import logging
 
 Base = declarative_base()
 
@@ -12,7 +13,8 @@ class User(Base):
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     name = Column(String(50))
     fullname = Column(String(50))
-    password = Column(String(12))
+    password = Column(String(40))
+    key = Column(String(32))
 
 
 class UserHandler(RestletHandler):
@@ -33,19 +35,21 @@ class UserHandler(RestletHandler):
         extensible = None  # None means no fields is extensible or a tuple with fields.
 
     @encoder('password')
-    def password_encoder(self, passwd, record=None):
+    def password_encoder(self, passwd, inst=None):
         import hashlib
         return hashlib.new('md5', passwd).hexdigest()
 
     @route(r'/(?P<uid>[0-9]+)/login', 'POST', 'PUT')
+    @route(r'/login', 'POST', 'PUT')
     def do_login(self, *args, **kwargs):
-        print "OK, It's done!: %s, %s" % (args, kwargs)
+        self.logger.info("OK, It's done!: %s, %s, %s", args, kwargs, self.request.arguments)
         self.write("OK, It's done!: %s, %s" % (args, kwargs))
 
 
 if __name__ == "__main__":
     import tornado.ioloop
+    logging.basicConfig(level=logging.DEBUG)
     application = Application([UserHandler.route_to('/users'), ],
-                              dburi='sqlite:///:memory:')
+                              dburi='sqlite:///:memory:', loglevel='DEBUG')
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
