798fb8b0544059960d64514b7839eaae3d52a5ac
Mingcai SHEN <archsh@gmail.com>
2013-11-22 19:05:44 +0800

    added serializing functions.

--- restlet/serializers.py
+++ restlet/serializers.py
@@ -1,29 +1,25 @@
+from sqlalchemy.orm.query import Query
 
-## JSON Serialization: application/json
-JSON_EXAMPLE = """
-{
-    "__ref": "http://api.example.com/v1/person?__limit=50&age__gt=18&__include_fields=fullname,age,gender",
-    "objects": [{"fullname": "AAAA", "age": 19, "gender": "Female"},
-                {},
-                {}],
-    "__count": 50,
-    "__total": 5000,
-    "__begin": 0,
-    "__model": "Person"
-}
-"""
 
-## YAML Serialization: application/x-yaml
-YAML_EXAMPLE = """
-__ref: http://api.example.com/v1/person?__limit=50&age__gt=18&__include_fields=fullname,age,gender
-objects:
-    - fullname: AAAA
-      age: 19
-      gender: Female
-    - fullname: ...
-    -
-__count: 50,
-__total: 5000,
-__begin: 0,
-__model: Person
-"""
+def serialize_object(inst, include_fields=None, exclude_fields=None, extend_fields=None):
+    """serialize_object: serialize a single object from model instance into a dictionary.
+    """
+    pass
+
+
+def serialize_query(inst, include_fields=None, exclude_fields=None, extend_fields=None):
+    """serialize_query: serialize a query into a list of object dictionary."""
+    assert isinstance(inst, Query)
+    inst.values()
+
+
+def serialize(inst, include_fields=None, exclude_fields=None, extend_fields=None):
+    """serialize: serialize model object(s) into dictionary(s)."""
+    if isinstance(inst, Query):
+        return serialize_query(inst, include_fields=include_fields,
+                               exclude_fields=exclude_fields,
+                               extend_fields=extend_fields)
+    else:
+        return serialize_object(inst, include_fields=include_fields,
+                                exclude_fields=exclude_fields,
+                                extend_fields=extend_fields)
