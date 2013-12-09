c8aac1fd989341ac004e4260c82cf8bfc2b23b1a
Mingcai SHEN <archsh@gmail.com>
2013-12-09 18:52:52 +0800

    Not using serialize_query when using Eager Loading...

--- restlet/serializers.py
+++ restlet/serializers.py
@@ -67,19 +67,18 @@ def serialize_query(cls, inst, include_fields=None, extend_fields=None):
     """serialize_query: serialize a query into a list of object dictionary."""
     if not isinstance(inst, Query):
         return None
-    include_fields = include_fields or cls.__table__.c.keys()
-    result = list(inst.values(*[getattr(cls, k) for k in include_fields]))
+    #include_fields = include_fields or cls.__table__.c.keys()
+    #result = list(inst.values(*[getattr(cls, k) for k in include_fields]))
     # TODO: Extend fields ....
-    return result
+    return serialize_object(cls, inst.all(), include_fields=include_fields, extend_fields=extend_fields)
 
 
 def serialize(cls, inst, include_fields=None, extend_fields=None):
     """serialize: serialize model object(s) into dictionary(s)."""
     if isinstance(inst, Query):
-        return serialize_query(cls, inst, include_fields=include_fields,
-                               extend_fields=extend_fields)
-    else:
-        return serialize_object(cls, inst, include_fields=include_fields,
-                                extend_fields=extend_fields)
+        inst = inst.all()
+        #return serialize_query(cls, inst, include_fields=include_fields,
+        #                       extend_fields=extend_fields)
+    return serialize_object(cls, inst, include_fields=include_fields, extend_fields=extend_fields)
 
 
