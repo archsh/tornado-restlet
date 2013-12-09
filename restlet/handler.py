edfbe99d87854624dfd0be93cb4b892b6ec95b35
Mingcai SHEN <archsh@gmail.com>
2013-12-09 15:51:23 +0800

    Move the serialize functions to serializers.py

--- restlet/handler.py
+++ restlet/handler.py
@@ -15,6 +15,7 @@ from tornado.log import access_log, app_log, gen_log
 #from tornado.util import bytes_type, unicode_type
 from . import exceptions
 from .helpers import simple_field_processor
+from .serializers import serialize, serialize_query, serialize_object
 try:
     import simplejson as json
 except:
@@ -348,37 +349,6 @@ def build_filter(model, key, value, joins=None):
         return build_filter(model.__mapper__.relationships[k1].mapper.class_, key, value, joins=joins)
     else:  # Check of this is
         return None, None
-
-
-def serialize_object(cls, inst, include_fields=None, extend_fields=None):
-    """serialize_object: serialize a single object from model instance into a dictionary.
-    """
-    if not isinstance(inst, cls):
-        return None
-    include_fields = include_fields or cls.__table__.c.keys()
-    result = dict([(k, getattr(inst, k)) for k in include_fields])
-    # TODO: Extend fields ....
-    return result
-
-
-def serialize_query(cls, inst, include_fields=None, extend_fields=None):
-    """serialize_query: serialize a query into a list of object dictionary."""
-    if not isinstance(inst, Query):
-        return None
-    include_fields = include_fields or cls.__table__.c.keys()
-    result = list(inst.values(*[getattr(cls, k) for k in include_fields]))
-    # TODO: Extend fields ....
-    return result
-
-
-def serialize(cls, inst, include_fields=None, extend_fields=None):
-    """serialize: serialize model object(s) into dictionary(s)."""
-    if isinstance(inst, Query):
-        return serialize_query(cls, inst, include_fields=include_fields,
-                               extend_fields=extend_fields)
-    else:
-        return serialize_object(cls, inst, include_fields=include_fields,
-                                extend_fields=extend_fields)
 
 
 def query_reparse(query):
--- restlet/serializers.py
+++ restlet/serializers.py
@@ -1,1 +1,34 @@
 from sqlalchemy.orm.query import Query
+
+
+def serialize_object(cls, inst, include_fields=None, extend_fields=None):
+    """serialize_object: serialize a single object from model instance into a dictionary.
+    """
+    if not isinstance(inst, cls):
+        return None
+    include_fields = include_fields or cls.__table__.c.keys()
+    result = dict([(k, getattr(inst, k)) for k in include_fields])
+    # TODO: Extend fields ....
+    return result
+
+
+def serialize_query(cls, inst, include_fields=None, extend_fields=None):
+    """serialize_query: serialize a query into a list of object dictionary."""
+    if not isinstance(inst, Query):
+        return None
+    include_fields = include_fields or cls.__table__.c.keys()
+    result = list(inst.values(*[getattr(cls, k) for k in include_fields]))
+    # TODO: Extend fields ....
+    return result
+
+
+def serialize(cls, inst, include_fields=None, extend_fields=None):
+    """serialize: serialize model object(s) into dictionary(s)."""
+    if isinstance(inst, Query):
+        return serialize_query(cls, inst, include_fields=include_fields,
+                               extend_fields=extend_fields)
+    else:
+        return serialize_object(cls, inst, include_fields=include_fields,
+                                extend_fields=extend_fields)
+
+
