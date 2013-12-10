c4baac1c88112405f0efc5e6d8235a172a2dae23
Mingcai SHEN <archsh@gmail.com>
2013-12-10 17:53:28 +0800

    updated.

--- restlet/__init__.py
+++ restlet/__init__.py
@@ -3,7 +3,7 @@ __author__ = 'Mingcai SHEN <archsh@gmail.com>'
 
 try:
     from .application import RestletApplication
-    from .handler import RestletHandler
-    __all__ = ['RestletApplication', 'RestletHandler']
+    from .handler import RestletHandler, encoder, generator, validator, route
+    __all__ = ['RestletApplication', 'RestletHandler', 'encoder', 'generator', 'validator', 'route']
 except:
     pass
